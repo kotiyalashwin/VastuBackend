07728b3fa06788a325d6c9d2a970fcca99bb5199
kotiyalashwin <ashwinkotiyal07@gmail.com>
2025-01-24 12:25:25 +0000

    zvaldiations

---
+++ prisma/migrations/20250124120617_anotations/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ProjectFloor" ALTER COLUMN "annotations" DROP NOT NULL;
--- src/validations/floorValidation.ts
+++ src/validations/floorValidation.ts
@@ -1,4 +1,6 @@
-import { z } from "zod";
+import { string, z } from "zod";
+
+const newRoom = z.object({});
 
 export const newFloorSchema = z.object({
   floorNumber: z.number(),
@@ -6,6 +8,7 @@ export const newFloorSchema = z.object({
   rawImg: z.string().url(),
   markedImg: z.string().url(),
   annotatedImg: z.string().url().optional(),
+  room: newRoom.array(),
 });
 
 export type newFloorEntities = z.infer<typeof newFloorSchema>;
