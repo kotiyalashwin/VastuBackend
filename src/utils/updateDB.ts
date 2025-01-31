f4de3be6eea0e865cb4977303a5b37f80a9aed54
kotiyalashwin <ashwinkotiyal07@gmail.com>
2025-01-31 17:53:01 +0000

    fix

--- src/utils/updateDB.ts
+++ src/utils/updateDB.ts
@@ -11,18 +11,19 @@ const updateDB = async (
   try {
     await prisma.projectFloor.update({
       where: {
-        id: floorId,
+        id: Number(floorId),
       },
       data: {
-        annotated_img: type === "annotated" ? link : null,
-        marked_img: type === "marked" ? link : null,
+        annotated_img: type === "annotated" ? link : "",
+        marked_img: type === "marked" ? link : "",
         modified: role === "CONSULTANT", // if consultant then modified is TRUE
       },
     });
 
     return;
-  } catch {
-    throw new Error(`Unable to update ${type} image requested by ${role}`);
+  } catch (error) {
+    console.log(error);
+    // throw new Error(`Unable to update ${type} image requested by ${role}`);
   }
 };
 
