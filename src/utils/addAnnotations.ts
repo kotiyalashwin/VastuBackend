c5e83c6ab7b8c57af339c234d000aae74f550c5d
ashwink02 <ashwinkotiyal07@gmail.com>
2025-02-25 14:49:53 +0530

    working annotations

--- src/utils/addAnnotations.ts
+++ src/utils/addAnnotations.ts
@@ -1,3 +1,5 @@
+import prisma from "../db";
+
 export const addAnnotations = async (floorid: number, annotaions: []) => {
   try {
     const floorId = floorid;
@@ -6,6 +8,11 @@ export const addAnnotations = async (floorid: number, annotaions: []) => {
       return { ...el, projectfloor: floorId };
     });
     console.log("final annotations", finalAnnotations);
+
+    // TODO -> Insert this array in the db in a single call
+    await prisma.annotations.createMany({
+      data: finalAnnotations,
+    });
   } catch {
     throw new Error("Error Adding Annotations");
   }
