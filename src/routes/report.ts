bcf2164145f41877ce52261f3ca533e3aff3cf7a
ashwink02 <ashwinkotiyal07@gmail.com>
2025-05-10 01:14:06 +0530

    combined report

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { generateReport, setReport } from "../controller/report";
+import { allReport, generateReport, setReport } from "../controller/report";
 
 const router = express.Router();
 
 router.post("/genreport/:floorId", generateReport);
 router.put("/submitreport/:floorId", setReport);
+router.get("/projectreport/:projectId", allReport);
 export default router;
